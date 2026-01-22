d6e0b7cbda434d9fa6eaf9d0635cba9d1010ae0a
Vladimir <vilenskivladimir@gmail.com>
2026-01-22 14:25:50 +0200

    Login Positive Test

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,16 +1,31 @@
 package ui_tests;
 
+import dto.User;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.LoginPage;
 
 import java.security.PublicKey;
 
 public class LoginTests extends AppManager {
     @Test
-    public void loginPositiveTest(){
+    public void loginPositiveTest() {
         System.out.println("first test");
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegForm("vil@gmail.com", "Qwerty123!");
+        loginPage.clickBtnLoginForm();
+    }
+
+    @Test
+    public void loginPositiveTestWithUser() {
+        User user = new User("vil@gmail.com", "Qwerty123!");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegFormWithUser(user);
+        loginPage.clickBtnLoginForm();
     }
 }
